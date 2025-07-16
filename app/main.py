dffb9797e6bd134f95148390ebbab9cddf92999c
jarvis0869 <vohrap.works.uwaterloo@gmail.com>
2025-07-16 23:47:41 +0530

    fix: corrected CORS allow_origins

--- app/main.py
+++ app/main.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
 from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],  # ⬅️ This is your frontend dev URL
+    allow_origins=["http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+from app.api import router as api_router
+app.include_router(api_router, prefix="/api"
+)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}       
