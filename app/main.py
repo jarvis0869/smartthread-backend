81ec6bb2ea2a2b1877aca22fb64b4c24191ef300
jarvis0869 <vohrap.works.uwaterloo@gmail.com>
2025-07-16 23:33:19 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # ⬅️ This is your frontend dev URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
